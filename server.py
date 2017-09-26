74055e8005b21c0f9114decf98ff8db081491f33
srhjne <sarahjaneiom@gmail.com>
2017-09-26 16:25:30 -0700

    added goal setting page

--- server.py
+++ server.py
@@ -3,7 +3,13 @@ from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
 from model import Monster, Attack, LevelLookup, User, Goal, GoalStatus, UserStatus
 from model import db, connect_to_db
+from oauth2client.client import OAuth2WebServerFlow
+import httplib2
+from apiclient.discovery import build
+import os
 import datetime
+
+import fitness_helper_functions as fhf
 
 
 app = Flask(__name__)
@@ -12,6 +18,15 @@ app = Flask(__name__)
 app.secret_key = "ABC"
 
 app.jinja_env.undefined = StrictUndefined
+
+client_id = os.environ["GGL_ID"]
+client_secret = os.environ["GGL_SECRET"]
+
+scope = "https://www.googleapis.com/auth/fitness.activity.read"
+flow = OAuth2WebServerFlow(client_id=client_id,
+                           client_secret=client_secret,
+                           scope=scope,
+                           redirect_uri='http://localhost:5000/auth_return/')
 
 
 @app.route('/user/<username>')
@@ -33,18 +48,162 @@ def profile_page(username):
 		hp = status.current_hp
 		level = status.level
 
-
+		progresses = []
 		goals = [goal for goal in user.goal if goal.valid_from < now and goal.valid_to > now]
-		progress = sorted([(status.date_recorded, status.value) for status in goals[-1].goalstatus])[0]
+		for goal in goals:
+			progress = sorted([(status.date_recorded, status.value, goal) for status in goal.goalstatus])[-1]
+			progresses.append(progress)
 
 
-		return render_template("user_page.html", username=username, goals=goals, 
-			                   goalstatus = progress, xp=xp, hp=hp, level=level)
+		return render_template("user_page.html", username=username,  
+			                   goalstatus = progresses, xp=xp, hp=hp, level=level)
 
 
 @app.route('/login')
 def login():
 	return render_template("login.html")
+
+
+
+@app.route('/auth_return/', methods=["GET"])
+def auth_ret():
+	code = request.args.get("code")
+	credentials = flow.step2_exchange(code)
+	http = httplib2.Http()
+	http = credentials.authorize(http)
+	service = build('fitness', 'v1', http=http)
+
+	if session.get("user_id"):
+		user = User.query.get(int(session.get("user_id")))
+		goals = [goal for goal in user.goal if goal.valid_from < datetime.datetime.now() and goal.valid_to > datetime.datetime.now()]
+		for goal in goals:
+			if goal.goal_type == "Steps":
+				millisstart = fhf.get_millis_date(goal.valid_from)
+				millisend = fhf.get_millis_date(goal.valid_to)
+				delta = millisend - millisstart
+				agg = service.users().dataset().aggregate(userId="me",
+				body={"aggregateBy":[{
+			    	"dataTypeName": "com.google.step_count.delta",
+			    	"dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+			  		}],
+			  		"bucketByTime": { "durationMillis": delta }, # This is 24 hours
+			  		"startTimeMillis": millisstart, #start time
+			  		"endTimeMillis": millisend # End Time
+			  		})
+
+
+			
+				if len(agg.execute()['bucket'][0]['dataset'][0]['point']) == 0:
+					return "No data"
+				else:
+					print "Hooray you have done %s Steps" % agg.execute()['bucket'][0]['dataset'][0]['point'][0]['value'][0]['intVal']
+					value = agg.execute()['bucket'][0]['dataset'][0]['point'][0]['value'][0]['intVal']
+					goalprogress = GoalStatus(goal_id = goal.goal_id, date_recorded = datetime.datetime.now(), value=value)
+					db.session.add(goalprogress)
+					db.session.commit()
+		return redirect("/user/%s" % user.username)
+	else:
+		flash("You need to log in")
+		return redirect("/login")
+
+@app.route('/test')
+def test():
+	auth_uri = flow.step1_get_authorize_url()
+	return redirect(auth_uri)
+
+
+@app.route('/set_goal', methods=['GET'])
+def set_goals():
+	if not session.get('user_id'):
+		flash("You must be logged in to view this")
+		return redirect("/login")
+	else:
+		return render_template("set_goals.html")
+
+
+
+@app.route("/calc_xp.json", methods=['GET', 'POST'])
+def calc_xp():
+	user = User.query.get(session.get('user_id'))
+	goal_type = request.args.get("goal_type")
+	goal_value = request.args.get("value")
+	valid_from = request.args.get("valid_from")
+	valid_to = request.args.get("valid_to")
+	value = request.args.get("value")
+	print "Position 2"
+	timedelta = (valid_to - valid_from).total_seconds()
+	scaled_value_test = goal_value/timedelta
+
+	max_xp = 500
+
+	goals = Goal.query.filter(Goal.goal_type == goal_type, Goal.user_id==user.user_id).all()
+	if not goals:
+		print "Position 3"
+		return jsonify({"xp": 500})
+	else:
+		print "Position 4"
+		for goal in goals:
+			timedelta = (goal.valid_to - goal.valid_from).total_seconds()
+			scaled_value = goal.value/timedelta
+			ratio = scaled_value/scaled_value_test
+			if ratio < 0.8:
+				print "Position 5"
+				return jsonify({"xp": 200})
+			elif ratio >= 0.8 and ratio < 1.1:
+				max_xp = 300
+		print "Position 6", max_xp
+		return jsonify({"xp":max_xp})
+
+
+
+
+
+
+@app.route('/set_goal', methods=['POST'])
+def set_goal_db():
+	if not session.get('user_id'):
+		flash("You must be logged in to view this")
+		return redirect("/login")
+	else:
+		user = User.query.get(session.get('user_id'))
+		goal_type = request.form.get("goal_type")
+		goal_value = request.form.get("value")
+		goal_value = int(goal_value)
+		valid_from_u = request.form.get("valid_from")
+		valid_from = datetime.datetime.strptime(valid_from_u, "%Y-%m-%d")
+		valid_to_u = request.form.get("valid_to")
+		valid_to = datetime.datetime.strptime(valid_to_u, "%Y-%m-%d")
+		
+		timedelta = (valid_to - valid_from).total_seconds()
+		scaled_value_test = goal_value/timedelta
+
+		xp = 500
+
+		goals = Goal.query.filter(Goal.goal_type == goal_type, Goal.user_id==user.user_id).all()
+		if not goals:
+			xp = 500
+		else:
+			for goal in goals:
+				timedelta = (goal.valid_to - goal.valid_from).total_seconds()
+				scaled_value = goal.value/timedelta
+				ratio = scaled_value/scaled_value_test
+				if ratio < 0.8:
+					xp = 200
+					break
+				elif ratio >= 0.8 and ratio < 1.1:
+					xp = 300
+			
+		goal = Goal(user_id=user.user_id, xp=xp, goal_type=goal_type, value=goal_value, valid_from=valid_from, valid_to=valid_to )
+		db.session.add(goal)
+		db.session.commit()
+		goal_db = Goal.query.filter(Goal.user_id==user.user_id, Goal.xp==xp, Goal.goal_type==goal_type, Goal.value==goal_value, Goal.valid_from==valid_from, Goal.valid_to==valid_to).first()
+		goalstatus = GoalStatus(goal_id=goal_db.goal_id,value=0,date_recorded=valid_from)
+		db.session.add(goalstatus)
+		db.session.commit()
+		return redirect("/user/%s"%user.username)
+
+
+
 
 
 @app.route('/login', methods=["POST"])
@@ -55,7 +214,9 @@ def login_post():
 	user = User.query.filter(User.username == username).one()
 	if user.password == password:
 		session["user_id"] = user.user_id
-		return redirect("/user/%s"%username)
+		auth_uri = flow.step1_get_authorize_url()
+		return redirect(auth_uri)
+		# return redirect("/user/%s"%username)
 	else:
 		flash("Incorrect Password")
 		return redirect("/login")

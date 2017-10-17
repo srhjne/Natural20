2d043f6aabfcfec3cda8ae511a7057d1d02c4edc
srhjne <sarahjaneiom@gmail.com>
2017-10-16 17:39:05 -0700

    got updated facebook api working :-)

--- static/facebook_post.js
+++ static/facebook_post.js
@@ -2,10 +2,20 @@
 
 
 
-$("#test_button").on("click", function(){FB.ui({
-  method: 'feed',
-  link: 'https://developers.facebook.com/docs/',
-  caption: 'An example caption',
-	}, function(response){
+
+$("#test_button").on("click", function(){
+  var FBDesc      = 'Come see my quests and join me on the adventure!';
+  var FBTitle     = 'I am currently battling to complete a quest on Natural 20';
+  var FBLink      = 'http://example.com/your-page-link';
+  var FBPic       = 'http://example.com/img/your-custom-image.jpg';
+
+  FB.ui({
+  method: 'share_open_graph',
+  action_type: 'og.shares',
+  action_properties: JSON.stringify({
+                        object: {
+                            'og:title': FBTitle,
+                            'og:description': FBDesc,}})
+                            }, function(response){
 		console.log("test")
 	})})
